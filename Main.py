a83936f4d81801142cda9d19f5c0917b3405d33d
bundasmanu <gustavo.abcdef@hotmail.com>
2019-10-22 15:53:25 +0100

    Atraves de GeoParse nao dá, o conteudo nao é concordante com o dataset, é necessário salvar o conteudo do dataset num ficheiro tab, e depois manipular os dados, e defini-lo de uma forma correta, num objeto Orange table

--- Main.py
+++ Main.py
@@ -6,10 +6,17 @@ from Orange.data.pandas_compat import table_from_frame
 from Bio import Geo
 
 def main():
-    dataset = GEOparse.get_GEO(geo="GDS360", destdir="./")
-    print(dataset.table)
+
+    '''
+        Assim era outra forma de fazer as coisas, mas o dataset nao esta a vir muito bem
+        dataset = GEOparse.get_GEO(geo="GDS360", destdir="./")
+        ds = ut.pandas_to_orange(dataset.table)
+        print(ds.X.shape)
+    '''
+
     data = Orange.data.Table("./datasetExplore")
-    print(data.X[0][9470:])
+    print(data.X.shape)
+    #print(data.X[0][9470:])
 
 if __name__== "__main__":
     main()
