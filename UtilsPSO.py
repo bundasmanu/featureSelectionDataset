3232d0708183ee49c42af6eadf972cb95da47631
bundasmanu <gustavo.abcdef@hotmail.com>
2019-10-23 00:21:30 +0100

    Funcao objetivo implementada

--- UtilsPSO.py
+++ UtilsPSO.py
@@ -1,4 +1,6 @@
-import UtilsDataset as uDT
+import UtilsDataset
+import UtilsClassifier
+import numpy as np
 
 class UtilsPSO():
 
@@ -85,8 +87,59 @@ class UtilsPSO():
     def setNNClassifier(self, newNNClassifier):
         self.neighborsClassifier = newNNClassifier
 
-    def objectiveFunction(self, posicoesBinariasParticula):
-        return None
+    def objectiveFunction(self, arrayParticleDimensions, dataset: UtilsDataset.UtilsDataset, classifier: UtilsClassifier.UtilsClassifier, alpha):
 
+        '''
+        All credits go to these documents:
+         - https://pyswarms.readthedocs.io/en/development/examples/feature_subset_selection.html;
+         - https://www.researchgate.net/publication/306927986_A_Feature_Selection_Method_Based_on_Modified_Binary_Coded_Ant_Colony_Optimization_Algorithm
+        :param arrayParticleDimensions: Array com as posicoes binarias de uma particula, tendo em consideracao as dimensoes do problema, em causa
+        :param dataset: Dataset a ser classificado
+        :param classifier: Objeto com os classificadores disponiveis
+        :return: custo/perda tendo em conta a funcao definida no artigo https://pyswarms.readthedocs.io/en/development/examples/feature_subset_selection.html;
 
+        Explicacao da funcao de custo:
+        TODOS OS CREDITOS DESTINAM-SE AO ARTIGO: Vieira, Mendoca, Sousa, et al. (2013)--> https://www.sciencedirect.com/science/article/abs/pii/S1568494613001361.
 
+        f(X)=α(1−P)+(1−α)(1−Nf/Nt)
+             - α: PARAMETRO PASSADO POR ARGUMENTO, ESTABELECENDO UMA RELACAO ENTRE AS RESTANTES VARIAVEIS DA FUNCAO;
+             - P: REPRESENTA A ACCURACY REFERENTE A PREVISAO DO CLASSIFICADOR UTILIZADO;
+             - Nf: Nº DE FEATURES RELEVANTES, QUE A PARTICULA ACREDITA QUE SAO AS NECESSARIAS À CLASSIFICACAO DO PROBLEMA;
+             - Nt: Nº TOTAL DE FEATURES DO PROBLEMA;
+        '''
+
+        featuresOfProblem = dataset.getDataset().X.shape[1]
+
+        #DEFINICAO DOS DADOS DO PROBLEMA, TENDO EM CONTA AS FEATURES QUE A PARTICULA CONSIDERA RELEVANTES PARA O PROBLEMA, SENDO EFETUADO O TREINO E PREVISAO, COM O DATASET ALTERADO (APENAS FEATURES RELEVANTES)
+
+        #SE A PARTICULA IDENTIFICAR QUE TODAS AS FEATURES SAO RELEVANTES ESNTAO O DATASET MANTEM-SE (FEATURES TODAS A 1)
+        if np.count_nonzero(arrayParticleDimensions) == 0:
+            X_subset = dataset.getDataset().X
+        else:
+            X_subset = dataset.getDataset().X[:, arrayParticleDimensions==1] #DATASET APENAS COM AS COLUNAS REFERENTES ÀS FEATURES QUE A PARTICULA ACHA RELEVANTES, PARA CADA UMA DAS 24 LINHAS
+
+        classifier.getSVMClassifier().fit(X_subset,dataset.getDataset().Y) #TREINO TENDO EM CONSIDERACAO APENAS AS FEATURES RELEVANTES, QUE A PARTICULA INDICOU
+
+        accuracy = (classifier.getSVMClassifier().predict(X_subset) == dataset.getDataset().Y).mean() #TESTE DO PROBLEMA, TENDO EM CONTA O TREINO EFETUADO ATRAS, E CALCULADA A ACCURACY, TENDO EM CONTA OS ACERTOS QUE EXISTIRAM
+
+        #CALCULO DA FUNCAO DE CUSTO, EXPLICADA ANTERIORMENTE
+        j = (alpha * (1 - accuracy) + (1 - alpha) * (1- (X_subset[1] /dataset.getDataset().X.shape[1])))
+
+        return j
+
+    def aplicarFuncaoObjetivoTodasParticulas(self, arrayParticleSDimensions, dataset: UtilsDataset.UtilsDataset, classifier: UtilsClassifier.UtilsClassifier, alpha):
+
+        '''
+
+        :param arrayParticlesDimensions: ARRAY BIDIMENSIONAL, REPRESENTANDO CADA PARTICULA, CONTENDO O VETOR DE DIMENSOES DE CADA UMA, REPRESENTANDO AS FEATURES QUE CADA UMA DELAS ACHA RELEVANTE PARA O PROBLEMA
+        :param dataset: DATASET DO PROBLEMA
+        :param classifier: CLASSIFICADOR A UTILIZAR NO TREINO E PREVISAO DO DATASET, TENDO EM CONTA AS FEATURES RELEVANTES, DE CADA PARTICULA
+        :param alpha: HIPERPARAMETRO ALPHA, QUE ESTABELECE UMA RELACAO ENTRE OS RESTANTES PARAMETROS DA FUNCAO DE CUSTO
+        :return: RESULTADO DO CUSTO DE CADA UMA DAS PARTICULAS
+        '''
+
+        nParticles = arrayParticleSDimensions.shape[0]
+
+        j = [self.objectiveFunction(arrayParticleSDimensions[i], dataset, classifier, alpha) for i in range(nParticles)] #ARRAY QUE AGREGA O RETORNO DA EXECUCAO DA FUNCAO OBJETIVO PARA CADA UMA DAS PARTICULAS
+
+        return j
